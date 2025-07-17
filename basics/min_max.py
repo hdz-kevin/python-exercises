bebb9d7a3604d5dcf5ba6623705e4f36a4048caa
hdz-kevin <kevinhdez592@gmail.com>
2025-07-17 10:13:05 -0600

    exercise 1: min max

---
+++ basics/min_max.py
@@ -0,0 +1,12 @@
+"""
+Valor minimo y maximo: Crea un programa que calcule el valor minimo y el valor maximo de tres numeros
+                        ingresados por el usuario.
+"""
+
+n1 = int(input("First number: "))
+n2 = int(input("Second number: "))
+n3 = int(input("Thirty number: "))
+
+print()
+print("The max number is: {}".format(max(n1, n2, n3)))
+print("The min number is: {}".format(min(n1, n2, n3)))
