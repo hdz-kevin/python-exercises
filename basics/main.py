daee21d84c2ac665952931032b15c602cfe2ccbc
hdz-kevin <kevinhdez592@gmail.com>
2025-07-17 10:11:04 -0600

    first commit

---
+++ basics/main.py
@@ -0,0 +1,9 @@
+# This is a sample Python script.
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hello, {name}')
+
+
+if __name__ == '__main__':
+    print_hi('World!')
